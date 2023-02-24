e6ac3834fb0078dc73ff0e9bcee543f38408caee
wade_barker <vadosz83@gmail.com>
2023-02-25 00:26:33 +0400

    add app controller and service to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UserModule } from './user/user.module';
 import { PostsModule } from './posts/posts.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -17,5 +18,6 @@ import { AppController } from './app.controller';
     PrismaModule,
   ],
   controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
